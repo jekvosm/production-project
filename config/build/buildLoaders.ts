14a31d7ef1a48ab8803db3e206388a3c2e2f8958
jekvosm <yn4k-jek@yandex.ru>
2024-04-21 21:39:35 +0400

    added react, react-dom, css-loader, sass-loader, component Counter

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,11 +1,23 @@
 import { RuleSetRule } from 'webpack'
 
 export function buildLoaders(): RuleSetRule[] {
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      'style-loader',
+      // Translates CSS into CommonJS
+      'css-loader',
+      // Compiles Sass to CSS
+      'sass-loader',
+    ],
+  }
+
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: 'ts-loader',
     exclude: /node_modules/,
   }
 
-  return [typescriptLoader]
+  return [typescriptLoader, cssLoader]
 }
