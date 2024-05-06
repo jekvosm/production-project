bcb73ef29bdc220564ac141ae4245b9506eb29fa
jekvosm <yn4k-jek@yandex.ru>
2024-05-06 21:52:45 +0400

    added ThemeSwitcher,  Navbar, Button, svgLoader, fileLoader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,11 @@ import { RuleSetRule } from 'webpack'
 import { BuildOptions } from './types/config'
 
 export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  }
+
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -31,5 +36,14 @@ export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
     exclude: /node_modules/,
   }
 
-  return [typescriptLoader, cssLoader]
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif|woff|woff2)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+      },
+    ],
+  }
+
+  return [fileLoader, svgLoader, typescriptLoader, cssLoader]
 }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,17 +3,17 @@ import { useTheme } from './providers/ThemeProvider'
 import './styles/index.scss'
 
 import { classNames } from 'shared/lib/classNames/classNames'
+
 import { AppRouter } from './providers/router'
 import { Navbar } from 'widgets/Navbar'
 
 const App = () => {
-  const { theme, toggleTheme } = useTheme()
+  const { theme } = useTheme()
 
   return (
     <div className={classNames('app', {}, [theme])}>
       <Navbar />
       <AppRouter />
-      <button onClick={toggleTheme}>Change Theme</button>
     </div>
   )
 }
