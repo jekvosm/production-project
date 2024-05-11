f9926f5f2a71b57f1bd2f15b0ef0fc7c69530a6d
jekvosm <yn4k-jek@yandex.ru>
2024-05-11 11:14:47 +0400

    added LangSwitcher

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,27 @@
+import { Suspense } from 'react'
+
+import { AppRouter } from './providers/router'
 import { useTheme } from './providers/ThemeProvider'
 
-import './styles/index.scss'
+import { Navbar } from 'widgets/Navbar'
+import { Sidebar } from 'widgets/Sidebar'
 
 import { classNames } from 'shared/lib/classNames/classNames'
 
-import { AppRouter } from './providers/router'
-import { Navbar } from 'widgets/Navbar'
-import { Sidebar } from 'widgets/Sidebar'
+import './styles/index.scss'
 
 const App = () => {
   const { theme } = useTheme()
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Navbar />
-      <div className='content-page'>
-        <Sidebar />
-        <AppRouter />
-      </div>
+      <Suspense fallback=''>
+        <Navbar />
+        <div className='content-page'>
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   )
 }
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 
 import { classNames } from 'shared/lib/classNames/classNames'
-import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher'
+import { LangSwitcher } from 'widgets/LangSwitcher/ui/LangSwitcher'
+import { ThemeSwitcher } from 'widgets/ThemeSwitcher'
 
 import cls from './Sidebar.module.scss'
 
@@ -25,7 +26,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
       <button onClick={onToggle}>toggle</button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
-        {/* LangSwitcher */}
+        <LangSwitcher className={cls.lang} />
       </div>
     </div>
   )
