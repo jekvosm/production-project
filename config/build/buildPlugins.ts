1294908c90b1ba0f21904d0affe762edf4af42fa
jekvosm <yn4k-jek@yandex.ru>
2024-05-11 11:37:28 +0400

    added hot-module-replacement-plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -20,5 +20,6 @@ export function buildPlugins({
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
+    isDev ? new webpack.HotModuleReplacementPlugin() : null,
   ]
 }
