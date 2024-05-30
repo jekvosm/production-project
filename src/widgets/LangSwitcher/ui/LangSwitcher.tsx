68e908f0d3e18e1c6224756968cdc33a4b53a2de
jekvosm <yn4k-jek@yandex.ru>
2024-05-30 14:01:12 +0400

    added jest, config for jest, added test for classNames function

--- src/widgets/LangSwitcher/ui/LangSwitcher.tsx
+++ src/widgets/LangSwitcher/ui/LangSwitcher.tsx
@@ -2,8 +2,6 @@ import { useTranslation } from 'react-i18next'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { Button, ThemeButton } from '../../../shared/ui/Button/Button'
 
-import cls from './LangSwitcher.module.scss'
-
 interface LangSwitcherProps {
   className?: string
 }
@@ -22,6 +20,7 @@ export const LangSwitcher = ({ className }: LangSwitcherProps) => {
       onClick={toggle}
     >
       {t('Язык')}
+      {t('sadasd')}
     </Button>
   )
 }
