f10d47791bb2a6b9b737256233bb4274eee911b8
jekvosm <yn4k-jek@yandex.ru>
2024-10-29 16:14:52 +0400

    upgraded sidebar, added button themes

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
+import { RoutePath } from 'shared/config/routeConfig/routeConfig'
 
 import { classNames } from 'shared/lib/classNames/classNames'
-import { Button } from 'shared/ui/Button/Button'
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
+import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button'
 import { LangSwitcher } from 'widgets/LangSwitcher/ui/LangSwitcher'
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher'
+
+import MainIcon from 'shared/assets/icons/main.svg'
+import AboutIcon from 'shared/assets/icons/about.svg'
 
 import cls from './Sidebar.module.scss'
 
@@ -27,12 +32,39 @@ export const Sidebar = ({ className }: SidebarProps) => {
         className,
       ])}
     >
-      <Button data-testid='sidebar-toggle' onClick={onToggle}>
-        {t('Переключить')}
+      <Button
+        data-testid='sidebar-toggle'
+        onClick={onToggle}
+        className={cls.collapseBtn}
+        theme={ButtonTheme.BACKGROUND_INVERTED}
+        square
+        size={ButtonSize.L}
+      >
+        {collapsed ? '>' : '<'}
       </Button>
+      <div className={cls.items}>
+        <AppLink
+          theme={AppLinkTheme.SECONDARY}
+          to={RoutePath.main}
+          className={cls.item}
+        >
+          <MainIcon className={cls.icon} />
+          <span className={cls.link}>{t('Главная')}</span>
+        </AppLink>
+
+        <AppLink
+          theme={AppLinkTheme.SECONDARY}
+          to={RoutePath.about}
+          className={cls.item}
+        >
+          <AboutIcon className={cls.icon} />
+          <span className={cls.link}>{t('О сайте')}</span>
+        </AppLink>
+      </div>
+
       <div className={cls.switchers}>
         <ThemeSwitcher />
-        <LangSwitcher className={cls.lang} />
+        <LangSwitcher short={collapsed} className={cls.lang} />
       </div>
     </div>
   )
